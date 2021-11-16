6a45009a23195913d04122e9ac78cf602eacdbd6
kim daewon <dan@kims-Mac-mini.local>
2021-11-16 21:51:28 +0900

    docs: add comment

--- devServer/webpack.config.js
+++ devServer/webpack.config.js
@@ -9,7 +9,17 @@ module.exports = {
         path: path.resolve(__dirname, "dist")
     },
     devServer: {
-        port: 9000,
+        proxy: {
+            /**
+             * domain을 설정하여 API 요청을 지정한 domain으로 보내도록 설정하면
+             * CORS 문제를 해결할 수 있다.
+             * 
+             * 만약 실제로 존재하지 않는 도메인 (eg. domain.com)을 사용한다면
+             * 아래의 옵션을 추가해줘야한다.
+             * changeOrigin: true
+             */
+            "/api": "http://localhost:3000"
+        }
     },
     plugins: [
         new HtmlWebpackPlugin({
